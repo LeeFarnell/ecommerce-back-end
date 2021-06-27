092400d021c7e6ce5c64e297b619711dbb5d9bef
Suraj Verma <surajverma@rentalcars.com>
2021-06-27 23:00:26 +0100

    refactor tag controllers

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,81 +1,85 @@
 const router = require("express").Router();
-const { Tag, Product, ProductTag } = require("../../models");
 
-// The `/api/tags` endpoint
+const { Tag, Product } = require("../../models");
+
+const productAttributes = [
+  "id",
+  "product_name",
+  "price",
+  "stock",
+  "category_id",
+];
 
 router.get("/", async (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
   try {
-    const allTags = await Tag.findAll({
+    const tags = await Tag.findAll({
       include: [
         {
           model: Product,
-          attributes: ["id", "product_name", "price", "stock", "category_id"],
+          attributes: productAttributes,
         },
       ],
     });
-    res.status(200).json(allTags);
+
+    res.status(200).json(tags);
   } catch (err) {
-    res.status(500).json(err);
-    console.log("Oops!");
+    console.log(err.message);
+    res.status(500).json({ error: "Failed to GET tags" });
   }
 });
 
 router.get("/:id", async (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
   try {
-    const tagData = await Tag.findByPk(req.params.id, {
+    const tag = await Tag.findByPk(req.params.id, {
       include: [
         {
           model: Product,
-          attributes: ["id", "product_name", "price", "stock", "category_id"],
+          attributes: productAttributes,
         },
       ],
     });
-    res.status(200).json(tagData);
+
+    res.status(200).json(tag);
   } catch (err) {
-    res.status(500).json(err);
-    console.log("Oops!");
+    console.log(err.message);
+    res.status(500).json({ error: "Failed to GET tag" });
   }
 });
 
 router.post("/", async (req, res) => {
-  // create a new tag
   try {
-    const newTag = await Tag.create(req.body);
-    res.status(200).json(newTag);
+    const tag = await Tag.create(req.body);
+
+    res.status(200).json(tag);
   } catch (err) {
-    res.status(500).json(err);
     console.log(err.message);
+    res.status(500).json({ error: "Failed to POST tag" });
   }
 });
 
 router.put("/:id", async (req, res) => {
-  // update a tag's name by its `id` value
   try {
-    const tagBody = req.body;
-    const updateTag = await Tag.update(tagBody, {
+    const updateTag = await Tag.update(req.body, {
       where: { id: req.params.id },
     });
+
     res.status(200).json(updateTag);
   } catch (err) {
-    res.status(500).json(err);
     console.log(err.message);
+    res.status(500).json({ error: "Failed to PUT tag" });
   }
 });
 
 router.delete("/:id", async (req, res) => {
-  // delete on tag by its `id` value
   try {
-    const deleteTag = await Tag.destroy({
+    const tag = await Tag.destroy({
       where: { id: req.params.id },
     });
-    res.status(200).json(deleteTag);
+
+    res.status(200).json(tag);
   } catch (err) {
-    res.status(500).json(err);
     console.log(err.message);
+    res.status(500).json({ error: "Failed to DELETE tag" });
   }
 });
 
